14565400207cad69642a09d90c1fef13fc9fc383
Jason <jasonyao58246@gmail.com>
2020-02-14 01:07:27 +1300

    Working email

--- src/js/submit.js
+++ src/js/submit.js
@@ -12,10 +12,17 @@ class sub extends React.Component{
   	message: ''
 	}
   }
-  handleSubmit( event ) {
-    event.preventDefault();
-    console.log(this.state);
-    this.resetForm();
+  handleSubmit(e) {
+    e.preventDefault();
+
+    axios({
+		method: "POST", 
+		url:"https://mailthis.to/jyao413@aucklanduni.ac.nz", 
+		data:  this.state
+	  })
+	console.log(this.state)
+	this.resetForm()
+
   }
   resetForm(){
     
@@ -24,23 +31,44 @@ class sub extends React.Component{
   
   render() {
 	return(
-  	<div className="App">
-  	<form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-  	<div className="form-group">
-      	<label htmlFor="name">Name</label>
-      	<input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-  	</div>
-  	<div className="form-group">
-      	<label htmlFor="exampleInputEmail1">Email address</label>
-      	<input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-  	</div>
-  	<div className="form-group">
-      	<label htmlFor="message">Message</label>
-      	<textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-  	</div>
-  	<button type="submit" className="btn btn-primary">Submit</button>
-  	</form>
-  	</div>
+  	// <div className="App">
+  	// <form action="https://mailthis.to/tigerj20" method="POST" encType="multipart/form-data">
+  	// <div className="form-group">
+    //   	<label htmlFor="name">Name</label>
+    //   	<input type="text" className="form-control" id="name"/>
+  	// </div>
+  	// <div className="form-group">
+    //   	<label htmlFor="exampleInputEmail1">Email address</label>
+    //   	<input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+  	// </div>
+  	// <div className="form-group">
+    //   	<label htmlFor="message">Message</label>
+    //   	<textarea className="form-control" rows="5" id="message" />
+  	// </div>
+  	// <button type="submit" className="btn btn-primary">Submit</button>
+  	// </form>
+  	// </div>
+
+
+	<div className="App">
+	<form action="https://mailthis.to/tigerj20" method="POST" encType="multipart/form-data">
+	<div className="form-group">
+		<label htmlFor="name">Name</label>
+	<input type="text" className="form-control" name="name" placeholder="Your name"/>
+	</div>
+	<div className="form-group">
+		<label htmlFor="exampleInputEmail1">Email address</label>
+		<input className="form-control" type="email" name="_replyto" placeholder="Your email"/>
+	</div>
+	<div className="form-group">
+		<label htmlFor="message">Message</label>
+		<textarea className="form-control" name="message" rows="5" placeholder="Enter your message here"></textarea>
+	</div>
+	<input type="hidden" name="_after" value="http://localhost:3000/"/>
+
+	<button type="submit" className="btn btn-primary">Submit</button>
+	</form>
+	</div>
 	);
   }
 
